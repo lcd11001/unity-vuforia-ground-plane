1dc769f7a18218834eec163e1f619431f1ce43a9
Luong Cong Dan <>
2018-07-13 14:45:49 +0700

    + project files

---
+++ Assets/ColliderTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderTrigger : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log("OnTriggerEnter " + other.name);
+		Rigidbody rb = other.GetComponent<Rigidbody>();
+		if (rb) {
+			rb.constraints |= RigidbodyConstraints.FreezePositionY;
+		}
+	}
+}
---
+++ Assets/ResetObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetObject : MonoBehaviour {
+	Vector3 originPos;
+	Quaternion originRot;
+	void Awake() {
+		originPos = gameObject.transform.localPosition;
+		originRot = gameObject.transform.localRotation;
+
+		// Fixed: Unity 2018.20f physic object will fall through the ground plane detection
+		FreezePostionY(true);
+	}
+	public void Reset() {
+		gameObject.transform.localPosition = originPos;
+		gameObject.transform.localRotation = originRot;
+	}
+
+	public void FreezePostionY(bool freeze) {
+		Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+		if (rb) {
+			if (freeze) {
+				rb.constraints |= RigidbodyConstraints.FreezePositionY;
+			} else {
+				rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+			}
+		}
+	}
+}
